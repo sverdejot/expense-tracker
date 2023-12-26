6d44eccad0090a8d0c0494e69ec8feca8aa73253
Samuel Verdejo <sverdejot@sverdehost>
2023-12-26 13:59:28 +0100

    test: fix naming and extensions

---
+++ Unit/Groups/Handler/Command/CreateRecordCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using Application;
+using Moq.AutoMock;
+
+namespace Unit;
+
+public class TestCreateRecordCommandHandler
+{
+    private readonly AutoMocker mocker = new AutoMocker();
+
+    private readonly CreateRecordCommandHandler handler;
+
+    public TestCreateRecordCommandHandler()
+    {
+        handler = mocker.CreateInstance<CreateRecordCommandHandler>();
+    }
+
+    [Fact]
+    public async Task Handle_ShouldAddRecordToGroup()
+    {
+        
+    }
+}
--- Unit/TestingExtensions.cs
+++ Unit/TestingExtensions.cs
@@ -7,9 +7,9 @@ public static class TestingExtensions
 {
     public static Faker<T> UsePrivateConstructor<T>(this Faker<T> faker)
         where T : class =>
-        faker.CustomInstantiator(f =>
-            Activator.CreateInstance(typeof(T), nonPublic: true) as T)
-         as Faker<T>;
+            faker.CustomInstantiator(f =>
+                (Activator.CreateInstance(typeof(T), nonPublic: true) as T)!)
+            as Faker<T>;
     public static Mapper GetMapper() =>
         new Mapper(Application.DependencyInjection.GetMappingConfiguration());
 }
